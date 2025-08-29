ae6c7f690eabbcf1c6c87e81648165b0b509e79f
Vanicoding27 <shivaani.6206@gmail.com>
2025-08-29 22:58:42 +0530

    new update

--- Schedule-project/src/main.jsx
+++ Schedule-project/src/main.jsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Cards from './Cards.jsx'
+import Card from './Card.jsx'
 import Button from './Button.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <>
     <App/>
-    <Cards/>
+    <Card/>
     <Button/>
   </>
 )
