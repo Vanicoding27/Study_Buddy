3e6021ed19cce75f2d792a703fe4a7738f833568
Vanicoding27 <shivaani.6206@gmail.com>
2025-10-23 16:06:53 +0530

    newupdate

--- Schedule-project/src/Button.jsx
+++ Schedule-project/src/Button.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-function Button({ placeholder }) {
+function Button({ placeholder, Click }) {
     return (
-        <button className="bg-blue-900 text-white m-5 px-4 py-2 rounded cursor-pointer hover:bg-blue-700 transition">
+        <button
+            id="askBtn"
+            onClick={Click}
+            className="bg-blue-900 text-white m-5 px-4 py-2 rounded cursor-pointer hover:bg-blue-700 transition">
             {placeholder}
         </button>
-    );
-}
-
-
-export default Button 
+    )
+};
+export default Button;
--- Schedule-project/src/Card.jsx
+++ Schedule-project/src/Card.jsx
@@ -4,7 +4,7 @@ function Card() {
   return (
     <div className="flex flex-row justify-center gap-6 mt-6">
       {/* Card 1 */}
-      <div className="bg-stone-900 p-6 rounded-xl shadow-lg w-62 text-center cursor-pointer hover:bg-stone-800 transition">
+      {/* <div className="bg-stone-900 p-6 rounded-xl shadow-lg w-62 text-center cursor-pointer hover:bg-stone-800 transition">
         <div className="flex justify-center mb-4">
           <span className="text-blue-400 text-2xl">⏱️</span>
         </div>
@@ -12,21 +12,21 @@ function Card() {
         <p className="text-gray-400 text-sm mt-2">
           Intelligent scheduling based on your available time slots
         </p>
-      </div>
+      </div> */}
 
       {/* Card 2 */}
-      <div className="bg-stone-900 p-6 rounded-xl shadow-lg w-62 text-center cursor-pointer hover:bg-stone-800 transition">
+      <div className="bg-black p-6 rounded-xl shadow-lg w-62 text-center cursor-pointer hover:bg-stone-800 transition">
         <div className="flex justify-center mb-4">
-          <span className="text-blue-400 text-2xl">⭐</span>
+          <span className="text-white text-2xl">⭐</span>
         </div>
         <h3 className="text-white text-lg font-semibold">Priority-Based</h3>
-        <p className="text-gray-400 text-sm mt-2">
+        <p className="text-white text-sm mt-2">
           Tasks organized by importance and urgency levels
         </p>
       </div>
 
       {/* Card 3 */}
-      <div className="bg-stone-900 p-6 rounded-xl shadow-lg w-62 text-center cursor-pointer hover:bg-stone-800 transition">
+      {/* <div className="bg-stone-900 p-6 rounded-xl shadow-lg w-62 text-center cursor-pointer hover:bg-stone-800 transition">
         <div className="flex justify-center mb-4">
           <span className="text-blue-400 text-2xl">📅</span>
         </div>
@@ -34,7 +34,7 @@ function Card() {
         <p className="text-gray-400 text-sm mt-2">
           Download your schedule as PDF for easy reference
         </p>
-      </div>
+      </div> */}
     </div>
   );
 }
